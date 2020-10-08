cc8dc83d6e528a5b53258ff0b9ae287caff5230a
tvfb85 <tvfb85@users.noreply.github.com>
2020-10-08 09:22:09 +0000

    automated commit by check50 [check50=True]

--- hello.c
+++ hello.c
@@ -3,6 +3,9 @@
 
 int main(void)
 {
+    // initialize variable called name which can hold data of type string, and set it to the return value of get_string()
     string name = get_string("What is your name?\n");
+
+    // print to the console
     printf("hello, %s\n", name);
 }

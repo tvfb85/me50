377c95c111ecbfdda757a15fc5a9cb3df3346467
tvfb85 <tvfb85@users.noreply.github.com>
2020-10-13 15:11:54 +0000

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -7,14 +7,14 @@ int main(void)
     long number = get_long("Number: ");
 
     // Set up variables to check the length of the creditcard number
-    long n;
-    n = number;
+    long digits;
+    digits = number;
     int length = 0;
 
     // Loop over the creditcard divding by ten and save a count of the digits
-    while (n)
+    while (digits)
     {
-        n /= 10;
+        digits /= 10;
         length++;
     }
 
@@ -22,37 +22,37 @@ int main(void)
     int checksum = 0;
 
     // Keep a track of the last number and remaining number for the checksum calculation
-    int lastNumber;
-    long remainingNumber = number;
+    int lastDigit;
+    long remainingDigits = number;
 
     // Initialize variables to save the first and second card digits to later check the card type
-    int firstNum;
-    int secondNum;
+    int firstDigit;
+    int secondDigit;
 
     // loop over the card number
     for (int i = 1; i <= length; i++)
     {
 
         // get the last number of the card
-        lastNumber = remainingNumber % 10;
+        lastDigit = remainingDigits % 10;
 
         // if the iteration index matches the length of the card, the first number is reached and saved
         if (i == length)
         {
-            firstNum = lastNumber;
+            firstDigit = lastDigit;
         }
 
         // if the iteration index is one less than the length of the card, the second number is reached and saved
         if (i == length - 1)
         {
-            secondNum = lastNumber;
+            secondDigit = lastDigit;
         }
 
 
         // if the remainder of the index modulo 2 is zero, we need to double the card number and save to the checksum
         if (i % 2 == 0)
         {
-            int doubled =  lastNumber * 2;
+            int doubled =  lastDigit * 2;
 
             // if the doubled number is double-figures, we add them together before adding the total to the checksum
             if (doubled > 9)
@@ -69,11 +69,11 @@ int main(void)
         // if the remainder of the index modulo 2 isn't zero, we do not multiple the card number
         else
         {
-            checksum += lastNumber;
+            checksum += lastDigit;
         }
 
         // update where we are in the loop
-        remainingNumber /= 10;
+        remainingDigits /= 10;
     }
 
     // initialize variable to save the result
@@ -84,20 +84,20 @@ int main(void)
     {
 
         // check for valid visa card
-        if (firstNum == 4 && (length == 13 || length == 16))
+        if (firstDigit == 4 && (length == 13 || length == 16))
         {
             result = "VISA\n";
         }
 
         // check for valid american express card
-        else if (firstNum == 3 && (secondNum == 4 || secondNum == 7) && length == 15)
+        else if (firstDigit == 3 && (secondDigit == 4 || secondDigit == 7) && length == 15)
         {
             result = "AMEX\n";
         }
         // check for valid mastercard
-        else if (firstNum == 5 && length == 16)
+        else if (firstDigit == 5 && length == 16)
         {
-            if (secondNum == 1 || secondNum == 2 || secondNum == 3 || secondNum == 4 || secondNum == 5)
+            if (secondDigit == 1 || secondDigit == 2 || secondDigit == 3 || secondDigit == 4 || secondDigit == 5)
             {
                 result = "MASTERCARD\n";
             }

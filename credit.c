687e52c82124c8050b439c9d30ff6069e7b98a30
tvfb85 <tvfb85@users.noreply.github.com>
2020-10-13 14:46:20 +0000

    automated commit by check50 [check50=True]

---
+++ credit.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int main(void)
+{
+    int length;
+    long number;
+    long n;
+
+    do
+    {
+        length = 0;
+        number = get_long("Number: ");
+        n = number;
+
+        while (n) {
+            n /= 10;
+            length++;
+        }
+    }
+    while (length < 13 || length > 16);
+
+    int checksum = 0;
+    int lastNumber;
+    long remainingNumber = number;
+
+    int firstNum;
+    int secondNum;
+
+    for (int i = 1; i <= length; i++)
+    {
+        lastNumber = remainingNumber % 10;
+        if (i == length)
+        {
+            firstNum = lastNumber;
+        }
+        if (i == length - 1)
+        {
+            secondNum = lastNumber;
+        }
+
+
+        if (i % 2 == 0)
+        {
+            int doubled =  lastNumber * 2;
+
+            if (doubled > 9)
+            {
+                int n1 = doubled % 10;
+                int n2 = doubled / 10;
+                checksum += (n1 + n2);
+            }
+            else {
+                checksum += doubled;
+            }
+        }
+        else
+        {
+            checksum += lastNumber;
+        }
+
+        remainingNumber /= 10;
+    }
+
+    string result;
+
+    if (checksum % 10 == 0)
+    {
+        if (firstNum == 4 && (length == 13 || length == 16))
+        {
+            result = "VISA\n";
+        }
+        else if (firstNum == 3 && (secondNum == 4 || secondNum == 7) && length == 15)
+        {
+            result = "AMEX\n";
+        }
+        else if (firstNum == 5 && length == 16)
+        {
+            if (secondNum == 1 || secondNum == 2 || secondNum == 3 || secondNum == 4 || secondNum == 5)
+            {
+                result = "MASTERCARD\n";
+            }
+            else
+            {
+                result = "INVALID\n";
+            }
+        }
+        else
+        {
+            result = "INVALID\n";
+        }
+    }
+    else {
+        result = "INVALID\n";
+    }
+
+    printf("%s\n", result);
+}

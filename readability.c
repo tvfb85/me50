ba6bf401b403fb788131397fd6bf080eb7eeaa25
tvfb85 <tvfb85@users.noreply.github.com>
2020-10-21 10:15:23 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -3,6 +3,7 @@
 #include <ctype.h>
 #include <math.h>
 
+// function declarations
 int count_letters(string text);
 int count_words(string text);
 int count_sentences(string text);
@@ -10,20 +11,26 @@ void print_grade(int grade);
 
 int main(void)
 {
+    // Ask user for text input
     string text = get_string("Text: ");
 
+    // Save return values after counting letters, words and sentences
     int letters = count_letters(text);
     int words = count_words(text);
     int sentences = count_sentences(text);
 
-    int L = (float) letters / words * 100;
-    int S = (float) sentences / words * 100;
+    // Calculate letters and sentences per 100 words and cast to float
+    float L = (float) letters / words * 100;
+    float S = (float) sentences / words * 100;
 
+    // Add calculated values to Coleman-Liau formula
     double index = 0.0588 * L - 0.296 * S - 15.8;
 
+    // Round grade level to nearest whole number and print
     print_grade(round(index));
 }
 
+// Count letters by iterating over string and incrementing the letters count for alphabetic characters only
 int count_letters(string text)
 {
     int letters = 0;
@@ -39,6 +46,7 @@ int count_letters(string text)
     return letters;
 }
 
+// Count words by iterating over string and checking for whitespaces
 int count_words(string text)
 {
     int words = 0;
@@ -51,9 +59,11 @@ int count_words(string text)
         }
     }
 
+    // Return words count plus one to account for no whitespace after the last word
     return words + 1;
 }
 
+// Count sentences by iterating over string and checking for periods, exclamation points and question marks
 int count_sentences(string text)
 {
     int sentences = 0;
@@ -69,6 +79,7 @@ int count_sentences(string text)
     return sentences;
 }
 
+// Check for lower than grade one and higher than grade 16 before outputting the result
 void print_grade(int grade)
 {
     if (grade < 1)

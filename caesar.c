23d9680c206068bdbe307b8077dbad365a15ada5
tvfb85 <tvfb85@users.noreply.github.com>
2020-10-21 10:25:32 +0000

    automated commit by submit50

--- caesar.c
+++ caesar.c
@@ -6,12 +6,14 @@
 
 int main(int argc, string argv[])
 {
+    // Validate argument count
     if (argc != 2)
     {
         printf("Usage: %s key\n", argv[0]);
         return 1;
     }
 
+    // Ensure key contains only digits
     for (int i = 0; i < strlen(argv[1]); i++)
     {
         if (!isdigit(argv[1][i]))
@@ -21,28 +23,36 @@ int main(int argc, string argv[])
         }
     }
 
+    // Convert string argument to integer
     int key = atoi(argv[1]);
 
+    // Ensure key is a positive integer
     if (key < 1)
     {
         printf("Usage: %s key\n", argv[0]);
         return 1;
     }
 
+    // Ask user for plaintext input
     string plaintext = get_string("plaintext: ");
 
+    // Begin output
     printf("ciphertext: ");
 
+    // Loop over plaintext
     for (int i = 0; i < strlen(plaintext); i++)
     {
 
+        // For all alphabetic characters apply the key, otherwise return the original character
         if (isalpha(plaintext[i]))
         {
+            // Use uppercase 'A' in ASCII to find the offset and transition by the key for uppercase letters
             if (isupper(plaintext[i]))
             {
                 int index = plaintext[i] - 65;
                 printf("%c", ((index + key) % 26) + 65);
             }
+            // Use lowercase 'a' in ASCII to find the offset and transition by the key for lowercase letters
             else
             {
                 int index = plaintext[i] - 97;
@@ -55,6 +65,7 @@ int main(int argc, string argv[])
         }
     }
 
+    // Finish with a newline and return success status code.
     printf("\n");
     return 0;
 }
